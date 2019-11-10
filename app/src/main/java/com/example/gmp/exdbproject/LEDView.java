4aa4c7d25978fe620315c9952664f271167e469d
heosinjae <fds5gqx@naver.com>
2019-11-10 20:57:49 +0900

    DB function Update & Add Reset Function

--- app/src/main/java/com/example/gmp/exdbproject/LEDView.java
+++ app/src/main/java/com/example/gmp/exdbproject/LEDView.java
@@ -6,9 +6,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +19,6 @@ import android.widget.ToggleButton;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import com.flask.colorpicker.ColorPickerView;
 import com.flask.colorpicker.OnColorSelectedListener;
@@ -44,8 +40,6 @@ public class LEDView extends AppCompatActivity {
 
     ImageButton colorSelect;
     ImageButton colorpic;
-
-    Boolean resetCheck = true;
 
     BluetoothSPP bt;
     Button load;
@@ -282,7 +276,6 @@ public class LEDView extends AppCompatActivity {
             }
             tableLayout.addView(tableRow);
         }
-
 
         load.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
