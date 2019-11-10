7e2583b81e7c28e001a218031958ee7eaafdd496
heosinjae <fds5gqx@naver.com>
2019-11-10 20:42:41 +0900

    DB function Update & Add Reset Function

--- app/src/main/java/com/example/gmp/exdbproject/LEDView.java
+++ app/src/main/java/com/example/gmp/exdbproject/LEDView.java
@@ -51,8 +51,7 @@ public class LEDView extends AppCompatActivity {
     Button load;
     Button reset;
 
-    ToggleButton btn;
-//    Drawable roundDrawable = ContextCompat.getDrawable(this, R.drawable.cerclebutton);
+    //    Drawable roundDrawable = ContextCompat.getDrawable(this, R.drawable.cerclebutton);
     int red = 255;
     int green = 255;
     int blue = 255;
@@ -61,9 +60,6 @@ public class LEDView extends AppCompatActivity {
     DatabaseReference rdb = fdb.getReference("user");
 
     ArrayList<String> points = new ArrayList<>();
-
-    TableRow tableRow;
-    TableLayout tableLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -88,7 +84,205 @@ public class LEDView extends AppCompatActivity {
         load = (Button) findViewById(R.id.Fianece2);
         reset = (Button) findViewById(R.id.buttonR);
 
-        setPixels(str);
+        final TableLayout tableLayout = (TableLayout) findViewById(R.id.table);
+        TableRow.LayoutParams tableParams = new TableRow.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
+
+        for (int i = 0; i < 8; i++) {
+            // Creation row
+            final TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(tableParams);
+            int position = i;
+
+            for(int j = 0 ; j < 32 ; j++){
+                final ToggleButton btn = new ToggleButton(this);
+                int setPos = i;
+                if(i==0 && (j%2 == 0)){
+                    if(j==0){
+                        setPos = i;
+                        position += 15;
+                    }
+                    else{
+                        setPos = position;
+                        position += 15;
+                    }
+
+                }
+                else if(i==0 && (j%2 == 1)){
+                    setPos = position;
+                    position += 1;
+                }
+
+                if(i==1 && (j%2 == 0)){
+                    if(j==0){
+                        setPos = i;
+                        position += 13;
+                    }
+                    else{
+                        setPos = position;
+                        position += 13;
+                    }
+                }
+                else if(i==1 && (j%2 == 1)){
+                    setPos = position;
+                    position += 3;
+                }
+
+                if(i==2 && (j%2 == 0)){
+                    if(j==0){
+                        setPos = i;
+                        position += 11;
+                    }
+                    else{
+                        setPos = position;
+                        position += 11;
+                    }
+                }
+                else if(i==2 && (j%2 == 1)){
+                    setPos = position;
+                    position += 5;
+                }
+
+                if(i==3 && (j%2 == 0)){
+                    if(j==0){
+                        setPos = i;
+                        position += 9;
+                    }
+                    else{
+                        setPos = position;
+                        position += 9;
+                    }
+                }
+                else if(i==3 && (j%2 == 1)){
+                    setPos = position;
+                    position += 7;
+                }
+
+                if(i==4 && (j%2 == 0)){
+                    if(j==0){
+                        setPos = i;
+                        position += 7;
+                    }
+                    else{
+                        setPos = position;
+                        position += 7;
+                    }
+                }
+                else if(i==4 && (j%2 == 1)){
+                    setPos = position;
+                    position += 9;
+                }
+
+                if(i==5 && (j%2 == 0)){
+                    if(j==0){
+                        setPos = i;
+                        position += 5;
+                    }
+                    else{
+                        setPos = position;
+                        position += 5;
+                    }
+                }
+                else if(i==5 && (j%2 == 1)){
+                    setPos = position;
+                    position += 11;
+                }
+
+                if(i==6 && (j%2 == 0)){
+                    if(j==0){
+                        setPos = i;
+                        position += 3;
+                    }
+                    else{
+                        setPos = position;
+                        position += 3;
+                    }
+                }
+                else if(i==6 && (j%2 == 1)){
+                    setPos = position;
+                    position += 13;
+                }
+
+                if(i==7 && (j%2 == 0)){
+                    if(j==0){
+                        setPos = i;
+                        position += 1;
+                    }
+                    else{
+                        setPos = position;
+                        position += 1;
+                    }
+                }
+                else if(i==7 && (j%2 == 1)){
+                    setPos = position;
+                    position += 15;
+                }
+
+                final int getPos = setPos;
+                btn.setId(setPos);
+                String text = Integer.toString(setPos);
+                btn.setTextOn(text);
+                btn.setTextOff(text);
+                btn.setLayoutParams(new TableRow.LayoutParams(150,150));
+                btn.setPadding(0,0,0,0);
+                btn.setBackgroundColor(Color.rgb(red,green,blue));
+
+                btn.setOnClickListener(new Button.OnClickListener(){
+                    public void onClick(View view){
+                        if(btn.isChecked()) {
+                            Toast.makeText(getApplicationContext(), "Clicked Position : " + getPos, Toast.LENGTH_SHORT).show();
+                            btn.setBackgroundColor(Color.rgb(red,green,blue));
+                            //points.add("R" + red + "G" + green + "B" + blue + "P" + getPos);
+                            rdb.child(str).child("RGBP").addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    rdb.child(str).child("RGBP").child("P"+getPos).setValue("R" + red + "G" + green + "B" + blue);
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+                                    Toast.makeText(getApplicationContext(), "DB error", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            if(bt.getServiceState() == BluetoothState.STATE_CONNECTED){
+                                String send = "P" + getPos + "R" + red + "G" + green + "B" + blue;
+                                //Toast.makeText(getApplicationContext(),send,Toast.LENGTH_SHORT).show();
+                                bt.send(send, true);
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(),"Bluetooth disconnected",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            //Toast.makeText(getApplicationContext(), "LED off", Toast.LENGTH_SHORT).show();
+                            btn.setBackgroundColor(Color.WHITE);
+                            rdb.child(str).child("RGBP").addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    rdb.child(str).child("RGBP").child("P"+getPos).removeValue();
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+                                    Toast.makeText(getApplicationContext(), "DB error", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            if(bt.getServiceState() == BluetoothState.STATE_CONNECTED){
+                                String del = "P" + Integer.toString(getPos) + "R0G0B0";
+                                //Toast.makeText(getApplicationContext(), del, Toast.LENGTH_SHORT).show();
+                                bt.send(del,true);
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(),"Bluetooth disconnected",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+
+                tableRow.addView(btn);
+            }
+            tableLayout.addView(tableRow);
+        }
+
 
         load.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
@@ -282,202 +476,7 @@ public class LEDView extends AppCompatActivity {
     }
 
     public void setPixels(final String str){
-        tableLayout = (TableLayout) findViewById(R.id.table);
-        TableRow.LayoutParams tableParams = new TableRow.LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);
-
-        for (int i = 0; i < 8; i++) {
-            // Creation row
-            tableRow = new TableRow(this);
-            tableRow.setLayoutParams(tableParams);
-            int position = i;
-
-            for(int j = 0 ; j < 32 ; j++){
-                btn = new ToggleButton(this);
-                int setPos = i;
-                if(i==0 && (j%2 == 0)){
-                    if(j==0){
-                        setPos = i;
-                        position += 15;
-                    }
-                    else{
-                        setPos = position;
-                        position += 15;
-                    }
-
-                }
-                else if(i==0 && (j%2 == 1)){
-                    setPos = position;
-                    position += 1;
-                }
-
-                if(i==1 && (j%2 == 0)){
-                    if(j==0){
-                        setPos = i;
-                        position += 13;
-                    }
-                    else{
-                        setPos = position;
-                        position += 13;
-                    }
-                }
-                else if(i==1 && (j%2 == 1)){
-                    setPos = position;
-                    position += 3;
-                }
-
-                if(i==2 && (j%2 == 0)){
-                    if(j==0){
-                        setPos = i;
-                        position += 11;
-                    }
-                    else{
-                        setPos = position;
-                        position += 11;
-                    }
-                }
-                else if(i==2 && (j%2 == 1)){
-                    setPos = position;
-                    position += 5;
-                }
-
-                if(i==3 && (j%2 == 0)){
-                    if(j==0){
-                        setPos = i;
-                        position += 9;
-                    }
-                    else{
-                        setPos = position;
-                        position += 9;
-                    }
-                }
-                else if(i==3 && (j%2 == 1)){
-                    setPos = position;
-                    position += 7;
-                }
-
-                if(i==4 && (j%2 == 0)){
-                    if(j==0){
-                        setPos = i;
-                        position += 7;
-                    }
-                    else{
-                        setPos = position;
-                        position += 7;
-                    }
-                }
-                else if(i==4 && (j%2 == 1)){
-                    setPos = position;
-                    position += 9;
-                }
-
-                if(i==5 && (j%2 == 0)){
-                    if(j==0){
-                        setPos = i;
-                        position += 5;
-                    }
-                    else{
-                        setPos = position;
-                        position += 5;
-                    }
-                }
-                else if(i==5 && (j%2 == 1)){
-                    setPos = position;
-                    position += 11;
-                }
-
-                if(i==6 && (j%2 == 0)){
-                    if(j==0){
-                        setPos = i;
-                        position += 3;
-                    }
-                    else{
-                        setPos = position;
-                        position += 3;
-                    }
-                }
-                else if(i==6 && (j%2 == 1)){
-                    setPos = position;
-                    position += 13;
-                }
-
-                if(i==7 && (j%2 == 0)){
-                    if(j==0){
-                        setPos = i;
-                        position += 1;
-                    }
-                    else{
-                        setPos = position;
-                        position += 1;
-                    }
-                }
-                else if(i==7 && (j%2 == 1)){
-                    setPos = position;
-                    position += 15;
-                }
-
-                final int getPos = setPos;
-                btn.setId(setPos);
-                String text = Integer.toString(setPos);
-                btn.setTextOn(text);
-                btn.setTextOff(text);
-                btn.setLayoutParams(new TableRow.LayoutParams(150,150));
-                btn.setPadding(0,0,0,0);
-
-                btn.setOnClickListener(new Button.OnClickListener(){
-                    public void onClick(View view){
-                        if(btn.isChecked()) {
-                            Toast.makeText(getApplicationContext(), "Clicked Position : " + getPos, Toast.LENGTH_SHORT).show();
-                            //points.add("R" + red + "G" + green + "B" + blue + "P" + getPos);
-                            rdb.child(str).child("RGBP").addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                    rdb.child(str).child("RGBP").child("P"+getPos).setValue("R" + red + "G" + green + "B" + blue);
-                                }
-
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError databaseError) {
-                                    Toast.makeText(getApplicationContext(), "DB error", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                            if(bt.getServiceState() == BluetoothState.STATE_CONNECTED){
-                                String send = "P" + getPos + "R" + red + "G" + green + "B" + blue;
-                                //Toast.makeText(getApplicationContext(),send,Toast.LENGTH_SHORT).show();
-                                bt.send(send, true);
-                            }
-                            else{
-                                Toast.makeText(getApplicationContext(),"Bluetooth disconnected",Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        else{
-                            //Toast.makeText(getApplicationContext(), "LED off", Toast.LENGTH_SHORT).show();
-                            rdb.child(str).child("RGBP").addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                    rdb.child(str).child("RGBP").child("P"+getPos).removeValue();
-                                }
-
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError databaseError) {
-                                    Toast.makeText(getApplicationContext(), "DB error", Toast.LENGTH_SHORT).show();
-                                }
-                            });
-                            if(bt.getServiceState() == BluetoothState.STATE_CONNECTED){
-                                String del = "P" + Integer.toString(getPos) + "R0G0B0";
-                                //Toast.makeText(getApplicationContext(), del, Toast.LENGTH_SHORT).show();
-                                bt.send(del,true);
-                            }
-                            else{
-                                Toast.makeText(getApplicationContext(),"Bluetooth disconnected",Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    }
-                });
-
-                tableRow.addView(btn);
             }
-            tableLayout.addView(tableRow);
-        }
-    }
 
 
 
