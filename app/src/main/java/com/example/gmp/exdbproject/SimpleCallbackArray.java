ec43bed2dd718d3e3973f20d713aef1f73f35686
heosinjae <fds5gqx@naver.com>
2019-11-12 19:43:59 +0900

    Add new interface

---
+++ app/src/main/java/com/example/gmp/exdbproject/SimpleCallbackArray.java
@@ -0,0 +1,8 @@
+package com.example.gmp.exdbproject;
+
+import java.util.ArrayList;
+
+public interface SimpleCallbackArray {
+    void callback(Object data);
+    void callbackArray(ArrayList<String> data);
+}
